bdb3863c094c914efc696da72a03647f258f306f
Edaz <edazpotato@gmail.com>
2021-10-24 16:15:22 +1300

    fix

--- next.config.js
+++ next.config.js
@@ -1,2 +1,2 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra({ future: { webpack5: true } })
+module.exports = withNextra({ webpack5: true })
